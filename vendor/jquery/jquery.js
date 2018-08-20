6c518e4fa4467827130be2c7166e9a6191c4213a
masonp <masonp@seattleu.edu>
2018-08-20 19:47:22 -0400

    working on map API

--- vendor/jquery/jquery.js
+++ vendor/jquery/jquery.js
@@ -11,13 +11,7 @@
  *
  * Date: 2018-01-20T17:24Z
  */
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 19.432608, lng: -99.133209},
-    zoom: 8
-  });
-}
+
 ( function( global, factory ) {
 
 	"use strict";
